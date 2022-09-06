ed1e5c48bd7a021b8a567080cb6783668d67b974
The-Oh-Fungi-Oh-Man <67115919+The-Oh-Fungi-Oh-Man@users.noreply.github.com>
2022-09-06 08:23:19 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Himbiewa Inc.",
+   "homepage": "himbiewa.github.io",
+   "sessionDataTimeoutTime": 2,
+   "sessionTimeoutTime": 2,
+   "termsOfService": "You must embrace the power of the Oh Fungi Oh and although you are not beneath the Master Kandelwib, you remain loyal to as will he to you...",
+   "kioskEnabled": true
+};
